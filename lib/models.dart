9b5dd80179b696c782841b17722fbd7d08bd7914
Nicolas Kijak <nkijak@gmail.com>
2020-05-09 00:04:01 -0400

    Adding code to make PushupController

--- lib/models.dart
+++ lib/models.dart
@@ -4,6 +4,19 @@ import 'package:flutter/foundation.dart';
 
 class Rep {
   DateTime when = DateTime.now();
+  int avgTime;
+  int count;
+  History history;
+  Rep.fromJSON(this.history, Map<String, dynamic> json) {
+    count = json['count'];
+    avgTime = json['avgTime'];
+  }
+  Rep(this.history, this.count, this.avgTime);
+
+  Map<String, dynamic> toJSON() => {
+    "count": count,
+    "avgTime": avgTime
+  };
 }
 
 class ExerciseSetEz{
@@ -53,6 +66,13 @@ class ExerciseSet {
   bool wrappedRests;
   ExerciseSet(this.counts, this.rests);
 
+  ExerciseSet.getPushupSetFor(int week, int day, Level level) {
+    print('getting pushups for week $week, day $day, level $level');
+    final schedule = PushupSchedule['week$week']['day$day'][level];
+    counts = schedule['counts'];
+    rests = schedule['rests'];
+  }
+
   int next() {
     int retval = -2;
     onCount = !onCount;
@@ -86,7 +106,7 @@ class ExerciseSet {
     return stillHasCounts || stillHasRests;
   }
 
-  bool isMax() => !onCount && countIndex == 0 && wrappedCounts;
+  bool get isMax => !onCount && countIndex == 0 && wrappedCounts;
 
   int getSetsToGo() {
     int totalSets = (counts.length > rests.length)? counts.length: rests.length+1;
@@ -130,12 +150,12 @@ class ExerciseSet {
 enum WorkoutType { PUSHUP, SITUP, SQUAT }
 enum Difficulty { EASY, MED, HARD }
 
-final int LONG_REST = 120000;
-final int MEDIUM_REST = 90000;
-final int STANDARD_REST = 60000;
-final int SHORT_REST = 45000;
+const int LONG_REST = 120000;
+const int MEDIUM_REST = 90000;
+const int STANDARD_REST = 60000;
+const int SHORT_REST = 45000;
 
-final PushupSchedule = {
+const PushupSchedule = {
   'week1': {
     'day1': {
       Difficulty.EASY: {
@@ -187,37 +207,66 @@ class Logg{
   History history;
   int week;
   int day;
+  List<Rep> _counts = [];
+
   Logg(this.history, this.week, this.day);
   Logg.fromJSON(this.history, Map<String, dynamic> json){
-    //TODO load json
+    week = json['week'];
+    day = json['day'];
+    List<Map<String, dynamic>> jsonCounts = json['counts']
+    _counts = jsonCounts.map((jsoncount) => Rep.fromJSON(history, jsoncount));
+
   }
+
+  List<int> get counts {
+    return _counts.map((rep) => rep.count);
+  }
+
+  int get totalCount =>
+      _counts.fold(0, (total, rep) => total + rep.count);
+
+  //TODO averageMillisPerPushup?
+  //TODO averagePushupFrequency?
+
   Map<String, dynamic> toJSON(){
-    //TODO implement
-    return {};
+    return {
+      "week": week,
+      "day": day,
+      "counts": counts.map((rep) => rep.toJSON())
+    };
   }
-  int get totalCount {
-    return 0;
+
+  Rep addCountAndTime(count, time) {
+    Rep rep = Rep(history, count, time);
+    _counts.add(rep);
+    return rep;
   }
+
+  bool isFor(week, day) =>
+      this.week == week && this.day == day;
 }
 
 class Level {
-  Map<String, dynamic> toJSON(){
-    //TODO implement
-    return {};
-  }
-}
-
-class GenericLevel extends Level {
   int index;
   int startWeek;
   String label;
+  Level(this.index, this.startWeek, this.label);
+  Map<String, dynamic> toJSON() => {
+    'index': index,
+    'startWeek': startWeek,
+    'label': label
+  };
+}
 
-  GenericLevel(this.index, this.startWeek, this.label);
-  GenericLevel.fromJson(Map<String, dynamic> json) {
-    index = json['index'];
-    label = json['label'];
-    startWeek = json['startWeek'];
-  }
+class GenericLevel extends Level {
+
+  GenericLevel(int index, int startWeek, String label):super(index, startWeek, label);
+
+  GenericLevel.fromJson(Map<String, dynamic> json):super(
+    json['index'],
+    json['label'],
+    json['startWeek']
+  );
 }
 
 
@@ -232,7 +281,9 @@ class History {
   bool finished;
   bool finalUnlocked;
 
-  History(String jsonHistory) {
+  History();
+
+  History.deserialize(String jsonHistory) {
     final json = jsonDecode(jsonHistory);
     day = json['day'];
     week = json['week'];
